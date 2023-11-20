dbadd6771f4de8128a6a1ca866b173980b33e97a
Romain Dyrac <romain.dyrac@gmail.com>
2023-11-20 23:13:30 +0100

    gallery

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom'
 
 export default function Card({ id, title, cover }) {
   return (
-    <Link to={`/accomodation/${id}`} className="card">
-      <img src={cover} alt={title} className="card_img" />
-      <h2>{title}</h2>
-    </Link>
+   
+      <div className="card">
+        <h2>{title}</h2>
+        <Link to={`/accomodation/${id}`}>
+          <img src={cover} alt={title} className="card_img"/>
+        </Link>
+      </div>
   )
 }
